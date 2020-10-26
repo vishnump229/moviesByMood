131b7f889d16b36da1a72c1ef8267d35774bfbc3
vishnu_mp <vishnump229@gmail.com>
2020-10-26 21:05:52 +0530

    bootstrap1

--- jscript.js
+++ jscript.js
@@ -1,6 +1,6 @@
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "200px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
